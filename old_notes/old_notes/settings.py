8dc64b2fc3a7dfcf646a6783c057ddb80b06cc7f
Jakub Chojak <kajoh@Jakubs-MacBook-Pro.local>
2020-07-30 11:14:38 +0200

    minor bug fixes

--- old_notes/old_notes/settings.py
+++ old_notes/old_notes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0(&quj-@dm#m*m8dhu&7263zghsst6eoaz@vx1z)dsu6n5^#k!$-6^e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
